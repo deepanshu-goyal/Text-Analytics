b80f6558291ab29be7520799951a11ca82293263
Deepanshu Goyal <deepanshu.goyal@gmail.com>
2019-12-20 17:46:15 +0530

    Add files via upload

--- Common_Functions.R
+++ Common_Functions.R
@@ -101,3 +101,58 @@ find_topics = function(dtm, k=10)
   car_document_top_topics = car_document %>% group_by(document) %>% top_n(1,gamma) %>% ungroup()
   print(car_document_top_topics)
 }
+
+# Function 6: Create COG graph
+
+distill.cog = function(dtm, # input dtm
+                       title="COG", # title for the graph
+                       central.nodes=4,    # no. of central nodes
+                       max.connexns = 5){  # max no. of connections  
+  
+  # first convert dtm to an adjacency matrix
+  dtm1 = as.matrix(dtm)   # need it as a regular matrix for matrix ops like %*% to apply
+  adj.mat = t(dtm1) %*% dtm1    # making a square symmatric term-term matrix 
+  diag(adj.mat) = 0     # no self-references. So diag is 0.
+  a0 = order(apply(adj.mat, 2, sum), decreasing = T)   # order cols by descending colSum
+  mat1 = as.matrix(adj.mat[a0[1:50], a0[1:50]])
+  
+  # now invoke network plotting lib igraph
+  library(igraph)
+  
+  a = colSums(mat1) # collect colsums into a vector obj a
+  b = order(-a)     # nice syntax for ordering vector in decr order  
+  
+  mat2 = mat1[b, b]     # order both rows and columns along vector b  
+  diag(mat2) =  0
+  
+  ## +++ go row by row and find top k adjacencies +++ ##
+  
+  wc = NULL
+  
+  for (i1 in 1:central.nodes){ 
+    thresh1 = mat2[i1,][order(-mat2[i1, ])[max.connexns]]
+    mat2[i1, mat2[i1,] < thresh1] = 0   # neat. didn't need 2 use () in the subset here.
+    mat2[i1, mat2[i1,] > 0 ] = 1
+    word = names(mat2[i1, mat2[i1,] > 0])
+    mat2[(i1+1):nrow(mat2), match(word,colnames(mat2))] = 0
+    wc = c(wc, word)
+  } # i1 loop ends
+  
+  
+  mat3 = mat2[match(wc, colnames(mat2)), match(wc, colnames(mat2))]
+  ord = colnames(mat2)[which(!is.na(match(colnames(mat2), colnames(mat3))))]  # removed any NAs from the list
+  mat4 = mat3[match(ord, colnames(mat3)), match(ord, colnames(mat3))]
+  
+  # building and plotting a network object
+  graph <- graph.adjacency(mat4, mode = "undirected", weighted=T)    # Create Network object
+  graph = simplify(graph) 
+  V(graph)$color[1:central.nodes] = "green"
+  V(graph)$color[(central.nodes+1):length(V(graph))] = "pink"
+  
+  graph = delete.vertices(graph, V(graph)[ degree(graph) == 0 ]) # delete singletons?
+  
+  plot(graph, 
+       layout = layout.kamada.kawai, 
+       main = title)
+  
+} # distill.cog func ends
